586f19744c409c21745698cb6b4d7f6a7d3ed14f
naoto67 <uver.com67@gmail.com>
2018-11-16 12:42:36 +0900

    add update and destroy action in schedule controller.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,7 +2,7 @@ class SchedulesController < ApplicationController
 
   def index
     # 必要なパラメータがないとき、status400でreturn
-    render json: { status: 400, message:  "パラメータが足りません."} and return if params[:year].nil?
+    render json: { status: 400, message: "bad request." } and return if params[:year].nil?
     schedules = Schedule.search_for_year_of(params[:year])
     render json: { status: 200, data: schedules }
   end
@@ -10,18 +10,28 @@ class SchedulesController < ApplicationController
   def create
     schedule = Schedule.new(schedule_params)
     if schedule.save
-      render json: { status: 200, message: "event was made successfully."}
+      render json: { status: 201, message: "event was made successfully."}
+    else
+      render json: { status: 400, message: "bad request." }
+    end
+  end
+
+  def update
+    schedule = Schedule.find(params[:id])
+    if schedule.update_attributes(schedule_params)
+      render json: { status: 201, message: "event was updated successfully." }
     else
       render json: { status: 400, message: "bad request."}
     end
   end
 
-  def update
-
-  end
-
   def destroy
-
+    schedule = Schedule.find(params[:id])
+    if schedule.destroy
+      render json: { status: 204, message: "event was deleted successfully." }
+    else
+      render json: { status: 500 }
+    end
   end
 
   private
