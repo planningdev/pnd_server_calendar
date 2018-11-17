041cfc840d4c91bfa43eceacb9bdddeaf9ab22cb
naoto67 <uver.com67@gmail.com>
2018-11-17 16:28:58 +0900

    update schedule model.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,59 +1,54 @@
 # frozen_string_literal: true
 
 class Schedule < ApplicationRecord
-  before_validation :prepare_save
+  before_validation :assign_all_day_time, if: :all_day?
 
   # title, start, finish, all_day は必ず存在する
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 128 }
   validates :start, presence: true
   validates :finish, presence: true
   # all_day は必ずtrueかfalseの値をとる
   validates :all_day, inclusion: { in: [true, false] }
+  validates :memo, presence: true, length: { maximum: 1028 }
 
   # 関数のvalidationを追加
   validate :finish_more_than_start
 
-  # 終わり時間は開始時間よりも遅くないといけない
-  def finish_more_than_start
-    return unless [start, finish].all?
-
-    # startがfinishより大きいときエラーを追加
-    if start > finish
-      errors.add(:finish, 'finish time must be more than start time.')
-    end
-  end
-
   # パラメータの前後6ヶ月分のイベントを返す
   def self.search_around_6_months(param)
     # パラメータをDateTime型にパース
-    date = DateTime.parse(param)
+    date = Time.zone.parse(param)
     # 検索の上限
-    upper = dateo + 6.month
+    upper = date + 6.months
     # 検索の下限
-    lower = date - 6.month
+    lower = date - 6.months
     # 上限と下限を用い、範囲内のイベントを返す
     where('start < ? and start > ?', upper, lower)
   end
 
   # パラメータの年分のイベントを返す
   def self.search_for_year_of(param)
-    # パラメータを数字にしてDateTime型を生成
-    date = DateTime.new(param.to_i)
-    # dateから年だけの情報を取得
-    year = date.year
     # scheduleモデルからyearで始まるイベントを返す
-    where('start like ?', "#{year}%")
+    where('start like ?', "#{param}%")
   end
 
-  # saveの直前(validationの直前)に行われる処理
+  private
+
+  # validationの直前に行われる処理
   # all_dayがtrueの時startとfinishを日の始まりと終わりに設定する
-  def prepare_save
+  def assign_all_day_time
     # startかfinishのどちらかが空だとreturn
     return unless [start, finish].all?
 
-    if all_day
-      self.start = start.beginning_of_day
-      self.finish = finish.end_of_day
-    end
+    self.start = start.beginning_of_day
+    self.finish = finish.end_of_day
+  end
+
+  # 終わり時間は開始時間よりも遅くないといけない
+  def finish_more_than_start
+    return unless [start, finish].all?
+
+    # startがfinishより大きいときエラーを追加
+    errors.add(:finish, 'finish time must be more than start time.') if start > finish
   end
 end
